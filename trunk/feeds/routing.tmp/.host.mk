e437b3917bf128d025a7a3318b3391378d30d068
superopen <lyj@lengyoujuntekiMacBook-Pro.local>
2013-10-20 16:24:53 +0800

    del feeds and tmp dir; update feeds.conf.default(switch to own git)
    ./scripts/feeds/update
    make package/symlinks

--- trunk/feeds/routing.tmp/.host.mk
+++
@@ -1,9 +0,0 @@
-HOST_OS:=Darwin
-HOST_ARCH:=x86_64
-GNU_HOST_NAME:=x86_64-apple-darwin12.4.0
-TAR:=/usr/bin/gnutar
-FIND:=/usr/bin/find
-BASH:=/bin/bash
-FIND_L=/usr/bin/find -L $(1)
-XARGS:=xargs
-PATCH:=/usr/bin/patch
